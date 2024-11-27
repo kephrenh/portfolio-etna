dbd367803804c2bf4326a6fede0d7641f56e64f2
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-11-27 16:48:20 +0100

    fix issue with all links active on first page load

--- src/js/active-navlink.js
+++ src/js/active-navlink.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const currentPath = window.location.pathname;
   links.forEach((link) => {
     const parentLi = link.parentElement;
+    link.classList.remove("active");
     if (link.href.includes(`${currentPath}`)) {
       link.classList.add("active");
       parentLi.classList.add("active");
